fec3bf277778045ff68cf8f58aec5b22ad0e7262
Chelsea Runacres <chelsea.runacres@gmail.com>
2022-11-10 21:02:35 -0500

    if statements for user input added

--- Assets/script.js
+++ Assets/script.js
@@ -8,24 +8,64 @@ var number = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 
 function generatePassword() {
-  var chooseLength = window.prompt("Please enter your desired password length.");
-  var chooseUpper = window.prompt("Should your password include uppercase letters?")
-  if (
-    chooseLength < 8 || chooseLength > 128) {
-    window.alert("Please choose a length between 8 and 128 characters.");
+  var chooseLength = prompt("Please enter your desired password length.");
+  var chooseLengthNumber = parseInt(chooseLength);
+
+  if (chooseLengthNumber < 8 || chooseLengthNumber > 128) {
+    alert("Please choose a length between 8 and 128 characters.");
   } 
-  
-  if (
-    chooseUpper 
-    
-    }
-// write my function here
-// includes prompts & if statements
+  console.log(chooseLength);
+
+  uppercase = confirm("Should your password contain uppercase letters?");
+  console.log(uppercase);
+  lowercase = confirm("Should your password contain lowercase letters?");
+  console.log(lowercase);
+  special = confirm("Should your password contain special characters?");
+  console.log(special);
+  number = confirm("Should your password contain numbers?");
+  console.log(number);
+
+ if (!uppercase && !lowercase && !special && !number){
+  alert("At least one type of character must be selected. Please try again");
+ }
+
+ var options = []
+
+ if (uppercase == true) {  
+  options.push(uppercase) 
+  console.log("hit")
+ }
+
+ if (lowercase == true) {
+  options.push(lowercase)
+ }
+
+ if (special == true) {
+  options.push(special)
+ }
+
+ if (number == true) {
+  options.push(number)
+ }
+ 
+ console.log(options)
+
+ var generatedPW = ""
+
+ for (var i = 0; i < chooseLengthNumber; i++) {
+
+ }
+
+//  if uppercase === true, those array values are added to the random mix
+// if (uppercase === true) {
+
+}
+
 
 // FOR loop to generate requested length of pw then 
 // eventually the return is that randomized output and not "password"
-  return "password"
-}
+  // return "password";
+
 
 // Write password to the #password input
 function writePassword() {
